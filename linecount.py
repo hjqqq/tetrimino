14bc15accbd4b4eb5dec7c68f0c101b5fe74a671
fatboy <thisisafatboy@gmail.com>
2012-05-31 21:50:51 +0800

    清理文件

--- linecount.py
+++ linecount.py
@@ -2,9 +2,17 @@
 
 import os, re
 
-searchDirs = ["header", "src"]
+menuDirs = ["header/menu", "src/menu"]
+soundDirs = ["header/sound", "src/sound"]
+gameDirs = ["header/game", "src/game"]
+globaldataDirs = ["header/globaldata", "src/globaldata"]
+utilityDirs = ["header/utility", "src/utility"]
+
+allDirs = ["header", "src"]
+
 filenameRegex = ".*\.(cpp|h)$"
 count = 0
+totalCount = 0;
 
 def countFile(filename):
     f = open(filename)
@@ -18,13 +26,42 @@ def countDirs(dirs):
         else:
             if re.match(filenameRegex, currentDir):
                 count += countFile(currentDir)
-                print currentDir
+                #print currentDir
         
 def main():
+    global count
+    
     currentDir = os.getcwd()
-    dirs = [currentDir + "/" + d for d in searchDirs]
+    
+    dirs = [currentDir + "/" + d for d in menuDirs]
     countDirs(dirs)
-    print "total lines: ", count
+    print "total menu lines:", count;
+    count = 0
+
+    dirs = [currentDir + "/" + d for d in soundDirs]
+    countDirs(dirs)    
+    print "total sound lines:", count;
+    count = 0
+
+    dirs = [currentDir + "/" + d for d in gameDirs]
+    countDirs(dirs)    
+    print "total game lines:", count;
+    count = 0
+
+    dirs = [currentDir + "/" + d for d in globaldataDirs]
+    countDirs(dirs)    
+    print "total globaldata lines:", count;
+    count = 0
+
+    dirs = [currentDir + "/" + d for d in utilityDirs]
+    countDirs(dirs)    
+    print "total utilitydata lines:", count;
+    count = 0
+
+    dirs = [currentDir + "/" + d for d in allDirs]
+    countDirs(dirs)    
+    print "total lines:", count;
+    
     return 0
 
 if __name__ == "__main__":
